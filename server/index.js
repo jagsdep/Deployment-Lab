3130f0225df085279b3d46cc0d3b11652bb86adb
Jagruti <jagruti.depan@gmail.com>
2021-11-17 15:08:59 -0600

    pushed

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })//normal get endpoint first parameter is backslash/2nd is callback function. Inside of the call back function is is we are responding by sending a file and inside the paranthesis the file that were sending, absolute file path. path.join will return the absolute file path and return index.htm,l which is relative file path, dirname is index.js file
 
-app.use('/css', express.static(path.join(__dirname, 'public/stylesheets')))
-app.use('/img',express.static(path.join(__dirname, 'public/images')));
+app.use('/css', express.static(path.join(__dirname, '../style.css')))//../ means look in my parent and find css file inside it
+
 
 
 
